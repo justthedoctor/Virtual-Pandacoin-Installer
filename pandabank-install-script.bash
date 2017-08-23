c6b091d8c295197b496bb64e41c30b53dcbfdd82
justthedoctor <justthedoctor.931@gmail.com>
2017-08-23 14:28:33 -0500

    VNC AutoStart

--- pandabank-install-script.bash
+++ pandabank-install-script.bash
@@ -59,7 +59,6 @@ sleep 5s
 apt-get -y install qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools build-essential libboost-dev libboost-all-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev git libminiupnpc-dev libboost1.55*
 echo  -e "${YELLOW}${BOLD}|| Cloning Pandabank from Github ||${NC}"
 sleep 5s
-# Still needs to be tested to ensure properly working.
 su - $sudousername -c "git clone https://github.com/digitalpandacoin/pandacoin/"
 echo  -e "${YELLOW}${BOLD}|| Compile PandaBank ||${NC}"
 sleep 5s
@@ -74,4 +73,42 @@ echo -e "${YELLOW}${BOLD} || Downloading & Installing most recent Blockchain ||$
 sleep 5s
 su - $sudousername -c "cd ~/.pandacoin/ && wget http://files.cryptodepot.org/.installer/database.tar.gz && tar zxvf database.tar.gz"
 su - $sudousername -c "rm -rf ~/.pandacoin/database.tar.gz"
+# Still needs to be tested to ensure properly working.
+echo '' >> /usr/local/bin/myvncserver
+echo '#!/bin/bash' >> /usr/local/bin/myvncserver
+echo 'PATH="$PATH:/usr/bin/"' >> /usr/local/bin/myvncserver
+echo 'DISPLAY="1"' >> /usr/local/bin/myvncserver
+echo 'DEPTH="16"' >> /usr/local/bin/myvncserver
+echo 'GEOMETRY="1024x768"' >> /usr/local/bin/myvncserver
+echo 'OPTIONS="-depth ${DEPTH} -geometry ${GEOMETRY} :${DISPLAY}"' >> /usr/local/bin/myvncserver
+echo '' >> /usr/local/bin/myvncserver
+echo 'case "$1" in' >> /usr/local/bin/myvncserver
+echo 'start)' >> /usr/local/bin/myvncserver
+echo '/usr/bin/vncserver ${OPTIONS}' >> /usr/local/bin/myvncserver
+echo ';;' >> /usr/local/bin/myvncserver
+echo '' >> /usr/local/bin/myvncserver
+echo 'stop)' >> /usr/local/bin/myvncserver
+echo '/usr/bin/vncserver -kill :${DISPLAY}' >> /usr/local/bin/myvncserver
+echo ';;' >> /usr/local/bin/myvncserver
+echo '' >> /usr/local/bin/myvncserver
+echo 'restart)' >> /usr/local/bin/myvncserver
+echo '$0 stop' >> /usr/local/bin/myvncserver
+echo '$0 start' >> /usr/local/bin/myvncserver
+echo ';;' >> /usr/local/bin/myvncserver
+echo 'esac' >> /usr/local/bin/myvncserver
+echo 'exit 0' >> /usr/local/bin/myvncserver
+chmod +x /usr/local/bin/myvncserver
+echo '[Unit]' >> /lib/systemd/system/myvncserver.service
+echo 'Description=Manage VNC Server on this droplet' >> /lib/systemd/system/myvncserver.service
+echo '' >> /lib/systemd/system/myvncserver.service
+echo '[Service]' >> /lib/systemd/system/myvncserver.service
+echo 'Type=forking' >> /lib/systemd/system/myvncserver.service
+echo 'ExecStart=/usr/local/bin/myvncserver start' >> /lib/systemd/system/myvncserver.service
+echo 'ExecStop=/usr/local/bin/myvncserver stop' >> /lib/systemd/system/myvncserver.service
+echo 'ExecReload=/usr/local/bin/myvncserver restart' >> /lib/systemd/system/myvncserver.service
+echo 'User=vnc' >> /lib/systemd/system/myvncserver.service
+echo '' >> /lib/systemd/system/myvncserver.service
+echo '[Install]' >> /lib/systemd/system/myvncserver.service
+echo 'WantedBy=multi-user.target' >> /lib/systemd/system/myvncserver.service
 echo -e "${YELLOW}%{BOLD} || Completed Enjoy your Virtual PandaBank, for further help please email: justthedoctor.931@gmail.com${NC}"
+
